a3b8f9f585b35a6d9aa512e67061e6ff1e31a149
Lexa0x <just.lexa0x@gmail.com>
2025-05-01 19:08:31 -0400

    authroutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/AuthController');
+const authController = require('../controllers/authController');
 const { check } = require('express-validator');
 const auth = require('../middlewares/auth');
 
