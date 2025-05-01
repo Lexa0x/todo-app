457d0a1c3856db4ca4e22b1c71f54a9556424f2d
Lexa0x <just.lexa0x@gmail.com>
2025-05-01 19:52:39 -0400

    adding api on task

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -6,7 +6,7 @@ const getAuthHeader = () => ({
   headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
 })
 
-export const getTasks = () => axios.get(`${API_URL}/tasks`, getAuthHeader())
-export const createTask = (task) => axios.post(`${API_URL}/tasks`, task, getAuthHeader())
-export const updateTask = (id, task) => axios.put(`${API_URL}/tasks/${id}`, task, getAuthHeader())
-export const deleteTask = (id) => axios.delete(`${API_URL}/tasks/${id}`, getAuthHeader())
+export const getTasks = () => axios.get(`${API_URL}/api/tasks`, getAuthHeader())
+export const createTask = (task) => axios.post(`${API_URL}/api/tasks`, task, getAuthHeader())
+export const updateTask = (id, task) => axios.put(`${API_URL}/api/tasks/${id}`, task, getAuthHeader())
+export const deleteTask = (id) => axios.delete(`${API_URL}/api/tasks/${id}`, getAuthHeader())
